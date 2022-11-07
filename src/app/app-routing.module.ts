72069412041d5f8c0511786e45785c0a04347818
wissemsamtouni <sa.wissem.11@gmail.com>
2022-11-07 15:05:37 +0100

    firstproject

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {FormComponent} from "./form/form.component";
+import {ListProductComponent} from "./list-product/list-product.component";
+import {NotFoundComponent} from "./not-found/not-found.component";
+import {DetailsComponent} from "./details/details.component";
+import {TodolistComponent} from "./todolist/todolist.component";
+import {CrudComponent} from "./crud/crud.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'acceuil',component:ListProductComponent},
+  {path:'ajouter',component:FormComponent},
+  {path:'todolist',component:TodolistComponent},
+  {path:'acceuil/:id',component:DetailsComponent},
+  {path:'crud',component:CrudComponent},
+//{path:'',redirectTo:'/acceuil',pathMatch:'full'} --> pour initialiser l'ouvertur sur cette path
+  {path:'**',component:NotFoundComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ListProductComponent } from './list-product/list-product.component';
+import { FooterComponent } from './footer/footer.component';
+import {FormsModule} from "@angular/forms";
+import { FormComponent } from './form/form.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { DetailsComponent } from './details/details.component';
+import { TodolistComponent } from './todolist/todolist.component';
+import { CrudComponent } from './crud/crud.component';
+import {HttpClientModule} from "@angular/common/http";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ListProductComponent,
+    FooterComponent,
+    FormComponent,
+    NotFoundComponent,
+    DetailsComponent,
+    TodolistComponent,
+    CrudComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
